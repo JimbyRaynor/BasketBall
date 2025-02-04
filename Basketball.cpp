0cee6b4ae7f00ad908909fef7f85a40218c4ea82
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2025-02-04 16:23:49 +1100

    Add files via upload

--- Basketball.cpp
+++ Basketball.cpp
@@ -8,9 +8,16 @@ int main()
    cout << "This program was originally written (in BASIC) by \n"
         << "Charles Bacheller of Dartmouth College shortly after 1963. \n"
         << "It appears in the book BASIC Computer Games by David Ahl (1978). \n\n"
-        << "* BASIC was created by John G. Kemeny and Thomas E. Kurtz at Dartmouth College (1963) \n"
-        << "* C was created by Dennis Ritchie at Bell Labs (1972-73) \n"
-        << "* C++ was created by Bjarne Stroustrup at Bell Labs (1979-1985)\n"
+        << "* BASIC was created by John G. Kemeny and Thomas E. Kurtz at Dartmouth College (1963). \n"
+        << "* C was created by Dennis Ritchie at Bell Labs (1972-73). \n"
+        << "* C++ was created by Bjarne Stroustrup at Bell Labs (1979-1985).\n"
         << "* The oldest high-level language is FORTRAN (1956). "
         << "  It was developed by a team led by John Backus at IBM. \n";
+   cout << "**************************************************\n"
+         << "This is Dartmouth College basketball. You will be Dartmouth "
+         << "captain and playmaker. \n"
+         << "Call shots as follows:\n"
+         << "1. Long (30 ft.) jump shot; \n";
+
+
 }
